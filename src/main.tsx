c6526d114c7091d57fa7bcd1b1950f23008706e0
waqie-vision <hafedhalsharafi2@gmail.com>
2026-07-17 00:37:23 +0300

    Enable Munjiz search and deadlines features

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { registerSW } from 'virtual:pwa-register'
-import App from './App'
+import AppV3 from './AppV3'
 import './styles.css'
+import './features.css'
 
 registerSW({ immediate: true })
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AppV3 />
   </StrictMode>
 )
